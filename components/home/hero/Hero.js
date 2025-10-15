89490cfaf747e74c8b9621ed90e8006ce0fa6f4f
Ahmed0702 <121123892+Ahmed0702@users.noreply.github.com>
2025-10-15 08:55:27 +0200

    Update Hero.js

--- components/home/hero/Hero.js
+++ components/home/hero/Hero.js
@@ -9,17 +9,17 @@ export const Hero = () => {
       <div className={styles.copyWrapper}>
         <Reveal>
           <h1 className={styles.title}>
-            Hey, I&apos;m Ahmed<span>.</span>
+            Hey, I&apos;m Ibrahim<span>.</span>
           </h1>
         </Reveal>
         <Reveal>
           <h2 className={styles.subTitle}>
-            I&apos;m a <span> UX/UI Designer and Front-End Developer  </span>
+            I&apos;m a <span> UX/UI Designer  </span>
           </h2>
         </Reveal>
         <Reveal>
           <p className={styles.aboutCopy}>
-Recent graduate with a strong background in front-end development and UX/UI design. Passionate about technology and problem-solving, I leverage my technical expertise to create intuitive, user-friendly digital experiences that seamlessly blend functionality and aesthetics.          <br /> 
+Recent graduate with a strong background in Product Design and UX/UI design. Passionate about technology and problem-solving, I leverage my technical expertise to create intuitive, user-friendly digital experiences that seamlessly blend functionality and aesthetics.          <br /> 
           Let&apos;s connect!😊🎨
           </p>
         </Reveal>
